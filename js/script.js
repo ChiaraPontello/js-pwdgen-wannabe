4c99a6085b8a6f51ab18565d300caeb9c8bddff6
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-08 10:16:27 +0200

    password

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const numeroFisso = 21;
+console.log(numeroFisso);
+
+let nome = prompt('Scrivi il tuo nome');
+console.log(nome);
+
+let cognome = prompt('Scrivi il tuo cognome');
+console.log(cognome);
+
+let colore = prompt('Scrivi il tuo colore preferito');
+console.log(colore);
+
+document.getElementById('password').innerHTML = nome + cognome + colore + numeroFisso;
+console.log(password);
